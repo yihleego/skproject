27efd412557cba4af61979997c0e02802bfc9f6e
leego <leegodev@hotmail.com>
2023-04-20 09:59:45 +0800

    Update Dockerfile

--- openapi/build.sh
+++ openapi/build.sh
@@ -3,4 +3,4 @@ docker rm   sk-openapi
 docker rmi  sk-openapi
 
 docker build -t sk-openapi .
-docker run -d -it -p 10000:10000 -e PROFILE="prod" --name=sk-openapi --network host sk-openapi
+docker run -d -it -p 10000:10000 --name=sk-openapi --network host sk-openapi
